3a441ba44dabe2ad629a7260312a1a32fe28496b
Emmanuel Raphael <aginaemmanuel6@gmail.com>
2024-09-18 18:15:04 +0200

    changes made

--- src/home/Review.jsx
+++ src/home/Review.jsx
@@ -34,136 +34,94 @@ const Review = () => {
           modules={[Autoplay, Pagination, Navigation]}
           className="mySwiper"
         >
-          
-            <SwiperSlide className="shadow-2xl bg-gray-200 py-8 px-4 md:m-5 rounded-lg border">
-              <div className="space-y-6 max-w-lg mx-auto"> {/* Reduced width */}
-                {/* Rating */}
-                <div className="text-amber-500 flex gap-2">
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
+          <SwiperSlide className="shadow-2xl bg-gray-200 py-8 px-4 md:m-5 rounded-lg border">
+            <div className="space-y-6 max-w-lg mx-auto">
+              {/* Rating */}
+              <div className="text-amber-500 flex gap-2">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
 
-                {/* Text Content */}
-                <div className="mt-7">
-                  <p className="mb-5 text-base text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Molestiae labore accusamus temporibus, consectetur magni
-                    veniam corrupti accusantium corporis enim libero veritatis,
-                    incidunt fuga nobis. Nemo doloremque quae quaerat asperiores
-                    voluptas quis aspernatur natus iste recusandae delectus
-                    aliquid fuga distinctio eum minus a illo nulla corporis,
-                    dolore incidunt aut qui exercitationem blanditiis veritatis
-                    iure. Iste eveniet quod nemo maxime sapiente, dolore dolorum
-                    incidunt ipsam. Nulla error delectus id consectetur architecto
-                    eaque quae magnam a distinctio, iste quaerat debitis quis,
-                    incidunt dolor fugiat nostrum minus. Dignissimos, velit.
-                    Minima eveniet maxime ad voluptate.
-                  </p>
+              {/* Text Content */}
+              <div className="mt-7">
+                <p className="mb-5 text-base text-gray-700">
+                  "I am absolutely in love with the hair products from this store!
+                  The quality is outstanding, and the customer service is excellent.
+                  I highly recommend them to anyone looking for reliable and stylish hair options."
+                </p>
 
-                  {/* Avatar and Author Info */}
-                  <div className="flex items-center gap-4">
-                    <Avatar
-                      img={RevPic}
-                      alt="avatar of Jese"
-                      rounded
-                      className="w-12 h-12"
-                    />
-                    <div>
-                      <h5 className="text-lg font-semibold">Basilia</h5>
-                      <p className="text-base text-gray-600">Nurse</p>
-                    </div>
+                {/* Avatar and Author Info */}
+                <div className="flex items-center gap-4">
+                  <Avatar img={RevPic} alt="avatar of Jese" rounded className="w-12 h-12" />
+                  <div>
+                    <h5 className="text-lg font-semibold">Basilia</h5>
+                    <p className="text-base text-gray-600">Nurse</p>
                   </div>
                 </div>
               </div>
-            </SwiperSlide>
-            <SwiperSlide className="shadow-2xl bg-gray-200 py-8 px-4 md:m-5 rounded-lg border">
-              <div className="space-y-6 max-w-lg mx-auto"> {/* Reduced width */}
-                {/* Rating */}
-                <div className="text-amber-500 flex gap-2">
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
+            </div>
+          </SwiperSlide>
 
-                {/* Text Content */}
-                <div className="mt-7">
-                  <p className="mb-5 text-base text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Molestiae labore accusamus temporibus, consectetur magni
-                    veniam corrupti accusantium corporis enim libero veritatis,
-                    incidunt fuga nobis. Nemo doloremque quae quaerat asperiores
-                    voluptas quis aspernatur natus iste recusandae delectus
-                    aliquid fuga distinctio eum minus a illo nulla corporis,
-                    dolore incidunt aut qui exercitationem blanditiis veritatis
-                    iure. Iste eveniet quod nemo maxime sapiente, dolore dolorum
-                    incidunt ipsam. Nulla error delectus id consectetur architecto
-                    eaque quae magnam a distinctio, iste quaerat debitis quis,
-                    incidunt dolor fugiat nostrum minus. Dignissimos, velit.
-                    Minima eveniet maxime ad voluptate.
-                  </p>
+          <SwiperSlide className="shadow-2xl bg-gray-200 py-8 px-4 md:m-5 rounded-lg border">
+            <div className="space-y-6 max-w-lg mx-auto">
+              {/* Rating */}
+              <div className="text-amber-500 flex gap-2">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
 
-                  {/* Avatar and Author Info */}
-                  <div className="flex items-center gap-4">
-                    <Avatar
-                      img={RevPic}
-                      alt="avatar of Jese"
-                      rounded
-                      className="w-12 h-12"
-                    />
-                    <div>
-                      <h5 className="text-lg font-semibold">Basilia</h5>
-                      <p className="text-base text-gray-600">Nurse</p>
-                    </div>
+              {/* Text Content */}
+              <div className="mt-7">
+                <p className="mb-5 text-base text-gray-700">
+                  "Amazing experience! The wig I purchased looks and feels so natural.
+                  I’ve received so many compliments and will definitely be coming back for more!"
+                </p>
+
+                {/* Avatar and Author Info */}
+                <div className="flex items-center gap-4">
+                  <Avatar img={RevPic} alt="avatar of Jese" rounded className="w-12 h-12" />
+                  <div>
+                    <h5 className="text-lg font-semibold">Sarah</h5>
+                    <p className="text-base text-gray-600">Teacher</p>
                   </div>
                 </div>
               </div>
-            </SwiperSlide>
-            <SwiperSlide className="shadow-2xl bg-gray-200 py-8 px-4 md:m-5 rounded-lg border">
-              <div className="space-y-6 max-w-lg mx-auto"> {/* Reduced width */}
-                {/* Rating */}
-                <div className="text-amber-500 flex gap-2">
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
+            </div>
+          </SwiperSlide>
 
-                {/* Text Content */}
-                <div className="mt-7">
-                  <p className="mb-5 text-base text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Molestiae labore accusamus temporibus, consectetur magni
-                    veniam corrupti accusantium corporis enim libero veritatis,
-                    incidunt fuga nobis. Nemo doloremque quae quaerat asperiores
-                    voluptas quis aspernatur natus iste recusandae delectus
-                    aliquid fuga distinctio eum minus a illo nulla corporis,
-                    dolore incidunt aut qui exercitationem blanditiis veritatis
-                    iure. Iste eveniet quod nemo maxime sapiente, dolore dolorum
-                    incidunt ipsam. Nulla error delectus id consectetur architecto
-                    eaque quae magnam a distinctio, iste quaerat debitis quis,
-                    incidunt dolor fugiat nostrum minus. Dignissimos, velit.
-                    Minima eveniet maxime ad voluptate.
-                  </p>
+          <SwiperSlide className="shadow-2xl bg-gray-200 py-8 px-4 md:m-5 rounded-lg border">
+            <div className="space-y-6 max-w-lg mx-auto">
+              {/* Rating */}
+              <div className="text-amber-500 flex gap-2">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
 
-                  {/* Avatar and Author Info */}
-                  <div className="flex items-center gap-4">
-                    <Avatar
-                      img={RevPic}
-                      alt="avatar of Jese"
-                      rounded
-                      className="w-12 h-12"
-                    />
-                    <div>
-                      <h5 className="text-lg font-semibold">Basilia</h5>
-                      <p className="text-base text-gray-600">Nurse</p>
-                    </div>
+              {/* Text Content */}
+              <div className="mt-7">
+                <p className="mb-5 text-base text-gray-700">
+                  "Hands down, the best hair products I've ever used! The texture,
+                  color, and style options are fantastic. Plus, the customer service team
+                  was incredibly helpful with my queries. Will be shopping again!"
+                </p>
+
+                {/* Avatar and Author Info */}
+                <div className="flex items-center gap-4">
+                  <Avatar img={RevPic} alt="avatar of Jese" rounded className="w-12 h-12" />
+                  <div>
+                    <h5 className="text-lg font-semibold">Anita</h5>
+                    <p className="text-base text-gray-600">Fashion Designer</p>
                   </div>
                 </div>
               </div>
-            </SwiperSlide>
+            </div>
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
